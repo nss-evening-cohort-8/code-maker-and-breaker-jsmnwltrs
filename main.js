be4498b8efdc47bf4c1ccf02329ed4691a0ca81f
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-22 13:37:10 -0500

    got foreach working instead of standard for loop

--- main.js
+++ main.js
@@ -11,18 +11,20 @@ convertCodeButton.addEventListener('click', () => {
     document.getElementById('codeString').innerHTML = '';
     let inputText = document.getElementById('wordText').value;
     let inputArray = inputText.split('');
-    for (i = 0; i < inputArray.length; i++) {
-    let outputStringCode = inputArray[i].charCodeAt(index) + ",";
+    // for (i = 0; i < inputArray.length; i++) {
+    inputArray.forEach(function(element){
+    let outputStringCode = element.charCodeAt(index) + ",";
     printToDom(outputStringCode, 'codeString')
-    }}
+    })}
 )
 
 convertWordButton.addEventListener('click', () => {
     document.getElementById('wordString').innerHTML = '';
     let inputTextCode = document.getElementById('codeText').value;
     let inputArrayCode = inputTextCode.split(',');
-    for (i = 0; i < inputArrayCode.length; i++) {
-    let outputStringWord = String.fromCharCode(inputArrayCode[i]);
+    // for (i = 0; i < inputArrayCode.length; i++) {
+    inputArrayCode.forEach(function(element){
+    let outputStringWord = String.fromCharCode(element);
     printToDom(outputStringWord, 'wordString')
-    }}
+})}
 )
